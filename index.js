66ca33404803cef11c38122a27162fdb60bea9dd
Akshay Shete <akshay.shete@ad.infosys.com>
2020-04-25 19:59:04 +0530

    GET & POST CALL

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 //Express is a framework to create api's. It is very robust, efficient, super high test coverage, HTTP Helpers
 
+//Run file as
+//node index.js -> it will start server then you can try as localhost:3000 with you api endpoint
+
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+
+//Here we are configuring Express to use body-parsers as middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Server Port -- Create Express Server
+const serverPort = 3000;
+app.listen(serverPort,function(){
+
+    console.log("Server has started on port "+serverPort);
+
+});
 
 //Creation of data
 const dataonServer = [
@@ -26,7 +45,6 @@ const dataonServer = [
 ];
 
 //Creation of end-points
-
 app.get('/api/userdetails',function(req,res){
 
     res.json(dataonServer);
@@ -49,14 +67,13 @@ app.get('/api/user/:id',function(req,res)
     }
 });
 
-//Server Port
-const serverPort = 3000;
-app.listen(serverPort,function(){
-
-    console.log("Server has started");
-
+app.get('/getHTML',function(req,res){
+    res.sendfile("index.html");
 });
 
-
-//Run file as
-//node index.js -> it will start server then you can try as localhost:3000 with you api endpoint
+app.post('/login',function(req,res){
+    var username=req.body.user;
+    var password=req.body.pass;
+    console.log("User name = "+username+", password = "+password);
+    res.end("done");
+  });
